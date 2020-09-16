5f68a392ae110098d9e427242ba87a9b0a90c6cb
Shashwat Reshme <reshmeshashwat@gmail.com>
2020-09-16 19:55:12 +0530

    Completed full could be modified later on

--- script.js
+++ script.js
@@ -20,15 +20,41 @@ function checkRequired(inputArr){
     }else{
         showSuccess(input);
     }
-})}
+});
+}
+function checkLength(input , min , max){
+    if (input.value.length < min){
+        showError(input , `${getFeildName(input)} must contain at least ${min} charecters `);
+    } else if(input.value.length > max){
+        showError(input , `${getFeildName(input)} must contain at most ${max} charecters `);
+    }else(
+        showSuccess(input)
+    )
+
+}
 function getFeildName (input){
     return input.id.charAt(0).toUpperCase() + input.id.slice(1);
 }
-function EmailValidation(input){
+function checkEmail(input){
     const re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    return re.test(input);
+    if (re.test(input.value.trim())) {
+        showSuccess(input);
+    } else {
+        showError(input, 'Email is not valid');
+}
+}
+function checkPasswordMatch(input1,input2){
+    if(input1.value !== input2.value){
+        showError(input2,"Passwords do not match")
+    }else{
+        showSuccess(input)
+    }
 }
 form.addEventListener('submit', function(e) {
     e.preventDefault();
     checkRequired([username, email, password, password2]);
-});
+    checkLength(username,3,15)
+    checkLength(password,8,25)
+    checkEmail(email)
+    checkPasswordMatch(password,password2)
+});   
